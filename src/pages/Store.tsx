954b5f3a55897ea58fa43db358dfffb0b34c329f
Nick Tse <nicholashtse@gmail.com>
2023-07-10 18:26:14 -0400

    import product images; display product details in store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,20 @@
+import { Row, Col } from 'react-bootstrap';
+import products from "../data/products.json";
+
 const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <h1>Store</h1>
+      {/* specifies # of columns and gap size for different screen sizes */}
+      <Row lg={3} md={2} xs={1} className='g-3'>
+        {products.map(product => {
+          return (
+            <Col>{JSON.stringify(product)}</Col>
+          )
+        })}
+      </Row>
+    </>
+  );
+};
 
-export default Store
+export default Store;
