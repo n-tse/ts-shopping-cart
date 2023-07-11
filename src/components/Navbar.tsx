c7f71456c122b71973d124b77e43f62a0e5eb45c
Nick Tse <nicholashtse@gmail.com>
2023-07-11 18:01:29 -0400

    add state&context to dynamically display total cart items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Container, Button, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
+
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -19,17 +22,32 @@ const Navbar = () => {
           </Nav.Link>
         </Nav>
         <Button
+          onClick={openCart}
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             height: "2.5rem",
             width: "2.5rem",
-            position: "relative"
+            position: "relative",
           }}
         >
           <BsCart3 style={{ flexShrink: 0 }} />
-          <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width:"1.2rem", height: "1.2rem", position: "absolute", bottom: 0, right: 0, transform: "translate(40%, 40%)", fontSize:"0.8rem"}}>2</div>
+          {cartQuantity !== 0 && <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.2rem",
+              height: "1.2rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(40%, 40%)",
+              fontSize: "0.8rem",
+            }}
+          >
+            {cartQuantity}
+          </div>}
         </Button>
       </Container>
     </NavbarBs>
