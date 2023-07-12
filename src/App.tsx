4b28a1aa6a8fa2170616e6327caf243fc2a324fc
Nick Tse <nicholashtse@gmail.com>
2023-07-12 14:46:59 -0400

    add basic homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
-import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
         </Routes>
       </Container>
     </ShoppingCartProvider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { Container, Button, Nav } from "react-bootstrap";
+import backgroundImage from "../../public/images/sarah-dorweiler-x2Tmfd1-SgA-unsplash.jpg";
+import "./Home.css";
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="hero-section">
+      <img
+        src={backgroundImage}
+        alt="Background"
+        className="background-image"
+      />
+      <div className="overlay">
+        <Container className="w-75">
+          <h1 className="hero-title">Welcome to Our Store</h1>
+          <p className="hero-description">
+            Discover a wide range of products at affordable prices. Shop now and
+            enjoy a seamless shopping experience.
+          </p>
+          <Button variant="outline-secondary" size="lg">
+            <Nav.Link to="/store" as={NavLink}>
+              Shop Now
+            </Nav.Link>
+          </Button>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
