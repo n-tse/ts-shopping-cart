e1f2b81e542873e78fda2e0a388c7bbbebe7f51e
Nick Tse <nicholashtse@gmail.com>
2023-07-19 13:06:35 -0400

    initialize server and stripe client

---
+++ server.js
@@ -0,0 +1,52 @@
+// sk_test_51NVcbdISQQoXlCSQ1T5HYN9Z7YddA4V063ZneBLqdYUh6HznUCZBM3AZ12qoChO7M7ebsjmEEojclaZyB534TdmI00KZZM7QWn
+// Luxury Handbag - Rosewood/Pink price_1NVcnHISQQoXlCSQGeljmcHP
+// Luxury Handbag - Yellow/Pink price_1NVcpXISQQoXlCSQz6AkBBTD
+// 15.6" FHD Laptop - price_1NVcqGISQQoXlCSQd65qVusW
+// Men's Wristwatch - price_1NVcsFISQQoXlCSQQBXR7xJD
+// Smart Watch - price_1NVcsjISQQoXlCSQNk4TTEIA
+// Running Shoes - price_1NVcteISQQoXlCSQujVlOqWu
+// Genuine Leather Couch - price_1NVcuEISQQoXlCSQdFDKmtg7
+// Premium Designer Shoes - price_1NVcuhISQQoXlCSQ5g0eDgh3
+// Over-ear Headphones - price_1NVcv8ISQQoXlCSQCj0D1JJA
+
+const express = require('express');
+const cors = require('cors');
+
+// initializes a stripe client for our specific account
+const stripe = require('stripe')('sk_test_51NVcbdISQQoXlCSQ1T5HYN9Z7YddA4V063ZneBLqdYUh6HznUCZBM3AZ12qoChO7M7ebsjmEEojclaZyB534TdmI00KZZM7QWn');
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+  console.log('req body: ', req.body);
+  const items = req.body.items;
+  // lineItems contains the data we want to send to stripe, formatted for stripe specifically
+  let lineItems = [];
+  items.forEach((item) => {
+    lineItems.push(
+      {
+        price: item.id,
+        quantity: item.quantity,
+      }
+    )
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+
+  res.send(JSON.stringify({
+    url: session.url
+  }));
+
+});
+
+const PORT = 5001;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
