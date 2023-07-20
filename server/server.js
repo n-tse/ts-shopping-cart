11c0b9151d5d3fcddd235d0586d920319369b9d9
Nick Tse <nicholashtse@gmail.com>
2023-07-20 16:13:51 -0400

    move server files to own directory

--- server/server.js
+++ server/server.js
@@ -48,6 +48,6 @@ app.post("/checkout", async (req, res) => {
 
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
